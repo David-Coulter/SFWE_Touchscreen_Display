aae422cb50b81bc9887f653a1ba835523840a4c9
Gerardo <jerryeagle@arizona.edu>
2022-11-17 14:55:25 -0700

    scratch

--- README.md
+++
@@ -1,1 +0,0 @@
-# SFWE301SemesterProject
--- mainMenu.java
+++
@@ -1,9 +0,0 @@
-public class mainMenu{
-	
-	public static void main(String[] args) {
-		
-		 mainMenuFrame mainmenuframe = new mainMenuFrame();	
-	}
-}
-
-	
